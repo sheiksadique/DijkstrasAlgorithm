92228ba3a31a165dc2952bf0a67458088a063e52
Sadique Sheik <sadique.sheik@synsense.ai>
2022-03-22 22:06:40 +0100

    Added doc strings to Edge

--- src/Edge.cpp
+++ src/Edge.cpp
@@ -5,10 +5,13 @@
 #include "Edge.h"
 #include "Node.h"
 
+// Default constructor
 Edge::Edge() {}
 
+// Constructor
 Edge::Edge(Node *source, Node *destination, double length) : source(source), destination(destination), length(length) {}
 
+// String representation of an edge
 std::ostream &operator<<(std::ostream &os, const Edge &edge) {
     os << "Edge: " << edge.source->getId() << " --> " << edge.destination->getId() << " length: " << edge.length;
     return os;
