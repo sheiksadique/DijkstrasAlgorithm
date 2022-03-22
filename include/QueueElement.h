907cc4e14704907d62c8a2e6eec680d78a815218
Sadique Sheik <sadique.sheik@synsense.ai>
2022-03-22 22:26:55 +0100

    QueueElement updated

--- include/QueueElement.h
+++ include/QueueElement.h
@@ -1,6 +1,10 @@
-//
-// Created by Sadique Sheik on 17.03.22.
-//
+/*
+ * Created by Sadique Sheik on 17.03.22.
+ * This template class is meant as a data structure
+ * holding the shortest known path from a source.
+ *
+ * I use a template class to extend its usability to other unforeseen use cases.
+*/
 
 #ifndef WEEK3_QUEUEELEMENT_H
 #define WEEK3_QUEUEELEMENT_H
@@ -14,19 +18,23 @@ public:
     double priority;
     std::list<T> path;
 
+    // Equality operator
     bool operator==(const QueueElement &rhs) const {
         return data == rhs.data;
     }
 
+    // Not equal operator
     bool operator!=(const QueueElement &rhs) const {
         return rhs != *this;
     }
 
+    // Greater than operator
     bool operator>(const QueueElement rhs) const {
         // The higher the priority, the smaller the value/distance
         return priority < rhs.priority;
     }
 
+    // Less than operator
     bool operator<(const QueueElement rhs) const {
         // The higher the priority, the smaller the value/distance
         return priority > rhs.priority;
--- src/QueueElement.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Sadique Sheik on 17.03.22.
-//
-
-#include "QueueElement.h"
